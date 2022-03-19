53824685ef5697f13bd00e2877585ed4058d0bc3
ar3d <info@ar3d.it>
2022-03-19 16:04:28 +0100

    Update places.js

--- JS/places.js
+++ JS/places.js
@@ -129,7 +129,7 @@ const clickListener = function (ev) {
         const label = document.createElement('span');
         const container = document.createElement('div');
         container.setAttribute('id', 'place-label');
-        label.innerHTML = "<DIV><a href="+link+"><img src="+sfondo+" target='_blank'><DIV>"+name+"<br>"+testo+"</DIV></a></DIV>";
+        label.innerHTML = "<a href="+link+"><img src="+sfondo+" target='_blank'>"+name+"<br>"+testo+"</a>";
         container.appendChild(label);
         document.body.appendChild(container);
 
