bc4583a32fec65dd5d0c8c6a6e59fc2f00ea2b04
ar3d <info@ar3d.it>
2022-03-20 10:31:28 +0100

    Update places-2.js

--- JS/places-2.js
+++ JS/places-2.js
@@ -126,9 +126,9 @@ const clickListener = function (ev) {
 	
     if (el && el === ev.target) {
         // after click, we are adding a label with the name of the place
-        const container = document.createElement('a');
+        const container = document.createElement('div');
 		container.setAttribute('id', 'animated-button1');
-        container.innerHTML = "href="+href+" target='_blank'><span></span><span></span><span></span><span></span>"+name+"<br>"+text+";
+        container.innerHTML = "<a href="+href+" target='_blank'><span></span><span></span><span></span><span></span>"+name+"<br>"+text+"</a>;
         
 		
 
